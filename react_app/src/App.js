8e0059e604e0b87796072a95f94be6ba4a7aba42
Karl Stencell <karldstencell@gmail.com>
2022-06-03 01:23:45 -0400

    nothing significant

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -7,7 +7,7 @@ import { updateCompanies } from './redux/companies';
 import Header from './Components/Header'
 // import CompaniesContainer from './Components/CompaniesContainer';
 import CompanyTile from './Components/CompanyTile'
-import gladiators from './assets/gladiators.png';
+import gladiators from './assets/gladiators-trimmed.png';
 import vs from './assets/vs-1.png';
 
 
@@ -37,16 +37,22 @@ function App() {
   return (
     <div className='app'>
       <Header/>
-      <Row className='d-flex body-container'>
-        <Col>
-          <CompanyTile/>
+      <Row className='d-flex body-container w-100 p-0 m-0'>
+        <Col className='col-sm-4 m-0 p-0'>
+          <CompanyTile className='m-0 p-0'/>
         </Col>
-        <Col>
-          <img className='verses' src={vs}></img>
-          <img className='gladiators'></img>
-        </Col>
-        <Col>
-          <CompanyTile/>
+        <Container className='image-container col-sm-4 m-0 p-0'>
+          <Row className='w-100 m-0 p-0 justify-content-center'>
+            {/* fuck */}
+            <img className='verses' src={vs}></img>
+          </Row>
+          <Row className='w-100 m-0 p-0 justify-content-center'>
+            {/* me */}
+            <img className='gladiators' src={gladiators}></img>
+          </Row>
+        </Container>
+        <Col className='col-sm-4 m-0 p-0'>
+          <CompanyTile className='m-0 p-0'/>
         </Col>
       </Row>
     </div>
