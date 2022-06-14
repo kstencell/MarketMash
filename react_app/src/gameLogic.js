1937a45db19a625b1a4e50225b96d8edab849856
Karl Stencell <karldstencell@gmail.com>
2022-06-13 21:32:50 -0400

    tried fixing play again animations

--- react_app/src/gameLogic.js
+++ react_app/src/gameLogic.js
@@ -7,20 +7,53 @@ export function DisplayResults(companies) {
     $("#left-scroll").removeClass("left-scroll-hover");
     $("#right-scroll").removeClass("right-scroll-hover");
 
-    $(".scroll-container").css("transition", "max-height 1s linear, transform 0.3s linear");
+    $(".scroll-container").css("transition", "max-height 1s linear, transform 0.7s linear");
+    $(".scroll-text").css("transition", "max-height 0.5s linear");
 
     if (companies[0]['market_cap'] >= companies[1]['market_cap']) {
-        $("#left-scroll").css("transform", "scale(1.05)");
-        $("#right-scroll").css("transform", "scale(0.9)");
+        $(function () {
+            $("#left-scroll").animate({
+                'max-height': '100%'
+            }, {duration: 500, queue: false}).css({
+                'transform': 'scale(1.05)'
+            });
+            $("#right-scroll").animate({
+                'max-height': '100%'
+            }, {duration: 500, queue: false}).css({
+                'transform': 'scale(0.9)'
+            });
+            $(".scroll-text").animate({
+                'max-height': '100%'
+            }, {duration: 500, queue: false});
+        });
+        // $("#left-scroll").css("transform", "scale(1.05)");
+        // $("#right-scroll").css("transform", "scale(0.9)");
     }
     else {
-        $("#left-scroll").css("transform", "scale(0.9)");
-        $("#right-scroll").css("transform", "scale(1.05)");
+
+        $(function () {
+            $("#right-scroll").animate({
+                'max-height': '100%'
+            }, {duration: 500, queue: false}).css({
+                'transform': 'scale(1.05)'
+            });
+            $("#left-scroll").animate({
+                'max-height': '100%'
+            }, {duration: 500, queue: false}).css({
+                'transform': 'scale(0.9)'
+            });
+            $(".scroll-text").animate({
+                'max-height': '100%'
+            }, {duration: 500, queue: false});
+        });
+        
+        // $("#left-scroll").css("transform", "scale(0.9)");
+        // $("#right-scroll").css("transform", "scale(1.05)");
     }
 
-    $(".scroll-text").css("max-height", "100%");
-    $("#left-scroll").css("max-height", "100%");
-    $("#right-scroll").css("max-height", "100%");
+    // $(".scroll-text").css("max-height", "100%");
+    // $("#left-scroll").css("max-height", "100%");
+    // $("#right-scroll").css("max-height", "100%");
 
     $(".play-again-btn").css("visibility", "visible");
     $(".play-again-btn").css("opacity", "100");
@@ -29,19 +62,62 @@ export function DisplayResults(companies) {
 
 export function PlayAgain() {
 
-    $("#left-scroll").css({"transform" : "scale(1)", "max-height" : "20%"});
-    $("#right-scroll").css({"transform" : "scale(1)", "max-height" : "20%"});
-    $(".scroll-text").css("max-height", "0%");
+    $(".scroll-container").css("transition", "max-height 0.5s linear, transform 0.2s linear");
+
+    $(() => {
+        $("#right-scroll").animate({
+            'max-height': '20%'
+        }, {duration: 500, queue: false}).css({
+            'transform': 'scale(1)'
+        });
+        $("#left-scroll").animate({
+            'max-height': '20%'
+        }, {duration: 500, queue: false}).css({
+            'transform': 'scale(1)'
+        });
+        $(".scroll-text").animate({
+            'max-height': '0%'
+        }, {duration: 500, queue: false});
+    }).promise().done(() => {
+        $(".scroll-container").css("transition", "max-height 0.2s linear, transform 0.1s linear");
+        $(".scroll-text").css("transition", "max-height 0.2s linear");
+
+        $("#right-scroll").animate({
+            'max-height': '35%'
+        }, {duration: 50, queue: false});
+        $("#left-scroll").animate({
+            'max-height': '35%'
+        }, {duration: 50, queue: false});
+        $(".scroll-text").animate({
+            'max-height': '35%'
+        }, {duration: 50, queue: false});
+    });
+
+    // CALL API FOR NEW TEXT DATA
+
+    // const myTimeout = setTimeout(() => {
+    //     $(".scroll-container").css("transition", "max-height 0.2s linear, transform 0.1s linear");
+    //     $(".scroll-text").css("transition", "max-height 0.2s linear");
+
+    //     $(function () {
+    //         $("#right-scroll").animate({
+    //             'max-height': '35%'
+    //         }, {duration: 50, queue: false});
+    //         $("#left-scroll").animate({
+    //             'max-height': '35%'
+    //         }, {duration: 50, queue: false});
+    //         $(".scroll-text").animate({
+    //             'max-height': '35%'
+    //         }, {duration: 50, queue: false});
+    //     });
+    // }, 1000);
 
     $(".play-again-btn").css("visibility", "hidden");
     $(".play-again-btn").css("opacity", "0");
 
-    $(".scroll-container").css("transition", "max-height 1s linear, transform 0.1s linear");
+    // $("#left-scroll").removeAttr("style");
+    // $("#left-scroll").addClass("left-scroll-hover");
 
-    $("#left-scroll").css("transform", "");
-    $("#left-scroll").addClass("left-scroll-hover");
-
-    $("#right-scroll").css("transform", "");
-    $("#right-scroll").addClass("right-scroll-hover");
-
+    // $("#right-scroll").removeAttr("style");
+    // $("#right-scroll").addClass("right-scroll-hover");
 }
